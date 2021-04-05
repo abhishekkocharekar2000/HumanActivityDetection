111ceaa41f639918b4fa3452e03689def7abd012
MADPROJECT-ar <madprojectar@gmail.com>
2021-04-05 11:15:29 +0530

    Walking and running and still motions detected.

--- app/src/main/java/com/example/humanactivitydetection/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/humanactivitydetection/ui/home/HomeFragment.java
@@ -1,10 +1,16 @@
 package com.example.humanactivitydetection.ui.home;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,35 +19,123 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.example.humanactivitydetection.BackgroundDetectedActivitiesService;
+import com.example.humanactivitydetection.Constants;
 import com.example.humanactivitydetection.R;
+import com.google.android.gms.location.DetectedActivity;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    Button track;
+    private String TAG = HomeFragment.class.getSimpleName();
+    BroadcastReceiver broadcastReceiver;
+
+    private TextView txtActivity, txtConfidence;
+    private ImageView imgActivity;
+    private Button btnStartTrcking, btnStopTracking;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        track = root.findViewById(R.id.track);
-        track.setOnClickListener(new View.OnClickListener() {
+
+        txtActivity = root.findViewById(R.id.txt_activity);
+        txtConfidence = root.findViewById(R.id.txt_confidence);
+        imgActivity = root.findViewById(R.id.img_activity);
+        btnStartTrcking = root.findViewById(R.id.btn_start_tracking);
+        btnStopTracking = root.findViewById(R.id.btn_stop_tracking);
+
+        btnStartTrcking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String test = track.getText().toString();
-                if(test.equals("Start Tracking")){
-                    Toast.makeText(getContext(),"Started Tracking",Toast.LENGTH_SHORT).show();
-                    track.setText("Stop Tracking");
-                }
-                else if(test.equals("Stop Tracking")){
-                    Toast.makeText(getContext(),"Stopped Tracking",Toast.LENGTH_SHORT).show();
-                    track.setText("Start Tracking");
-                }
-
+                startTracking();
             }
         });
+
+        btnStopTracking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopTracking();
+            }
+        });
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(Constants.BROADCAST_DETECTED_ACTIVITY)) {
+                    int type = intent.getIntExtra("type", -1);
+                    int confidence = intent.getIntExtra("confidence", 0);
+                    handleUserActivity(type, confidence);
+                }
+            }
+        };
+
         return root;
+    }
+    private void handleUserActivity(int type, int confidence) {
+        String label = getString(R.string.activity_unknown);
+        int icon = R.drawable.ic_still;
+
+        switch (type) {
+            case DetectedActivity.ON_FOOT: {
+                label = getString(R.string.activity_on_foot);
+                icon = R.drawable.ic_walking;
+                break;
+            }
+            case DetectedActivity.RUNNING: {
+                label = getString(R.string.activity_running);
+                icon = R.drawable.ic_running;
+                break;
+            }
+            case DetectedActivity.STILL: {
+                label = getString(R.string.activity_still);
+                break;
+            }
+            case DetectedActivity.WALKING: {
+                label = getString(R.string.activity_walking);
+                icon = R.drawable.ic_walking;
+                break;
+            }
+            case DetectedActivity.UNKNOWN: {
+                label = getString(R.string.activity_unknown);
+                break;
+            }
+        }
+
+        Log.e(TAG, "User activity: " + label + ", Confidence: " + confidence);
+
+        if (confidence > Constants.CONFIDENCE) {
+            txtActivity.setText(label);
+            txtConfidence.setText("Confidence: " + confidence);
+            imgActivity.setImageResource(icon);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        LocalBroadcastManager.getInstance(getContext()).registerReceiver(broadcastReceiver,
+                new IntentFilter(Constants.BROADCAST_DETECTED_ACTIVITY));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        LocalBroadcastManager.getInstance(getContext()).unregisterReceiver(broadcastReceiver);
+    }
+
+    private void startTracking() {
+        Intent intent = new Intent(getContext(), BackgroundDetectedActivitiesService.class);
+        getActivity().startService(intent);
+    }
+
+    private void stopTracking() {
+        Intent intent = new Intent(getContext(), BackgroundDetectedActivitiesService.class);
+        getActivity().stopService(intent);
     }
 }
